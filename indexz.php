b180c5eb9b7409eb0804df622e5d80abb678c792
zdani <zdani@php.uitclassroom.com>
2016-06-15 23:54:18 +0200

    gggg

--- indexz.php
+++ indexz.php
@@ -1,19 +1,10 @@
 <?php
-function find_answer($a, $b, $c){
-    $d = ($b * $b) - (4 * $a * $c);
-    if($d > 0) {
-        $x1 = (-$b + sqrt($d))/(2 * $a);
-        $x2 = (-$b - sqrt($d))/(2 * $a);
-        echo "Если дискриминант больше нуля, то X1=".$x1.", X2=".$x2;
-    }
-    elseif($d == 0){
-        $x =(-$b + sqrt($d))/(2 * $a);
-        echo "Если дискриминант равен нулю, то X=".$x;
-    }
-    else {
-        echo "Дискриминант меньше нуля, корней нет";    
-    }
+function cube_calculation ($a, $b, $c){
+($a * $b *$c);
+{
+echo 'Вывести результат $s';
 }
-$s = find_answer(0, 0, 0);
-echo $s;
+}
+$s= cube_calculation (5, 5, 5);
+    echo $s;
 ?>
